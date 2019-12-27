f12f79951ffdd433e926b202bccdaf99315c67ab
Nelson198 <nteixeira88@gmail.com>
2019-12-27 22:53:03 +0000

    #5 Commit: Relatório     --> Conclusão (50%): falta expecificar as questões de interesse.

--- script.R
+++ script.R
@@ -1,12 +1,12 @@
 db <- read.csv(file="database/crime.csv", header=T)
 
+# Remover variáveis que dizem respeito às coordenadas dos locais dos crimes.
+db <- db[, -(15:17), drop=FALSE]
+
 # Análise exploratória.
 names(db)
 str(db)
 summary(db)
-
-# Remover variáveis que dizem respeito às coordenadas dos locais dos crimes.
-db <- db[, -(15:17), drop=FALSE]
 
 # Nº de registos de crimes em cada mês desde 2015 até 2019.
 table(db$MONTH, db$YEAR)
